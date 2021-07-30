850109376f45f09aa3124747ae3dd5375e541442
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2021-07-30 17:59:06 +0900

    commit

---
+++ src/test/java/com/example/demospringsecurityform/AccountControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demospringsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index_anonymous() throws Exception{
+        mockMvc.perform(get("/").with(user("guhyeon").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
